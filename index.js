d27f3a375e128df86625604bcb922ff94f70ffe0
IsrakUAP <israk2301@gmail.com>
2023-11-29 17:13:51 +0600

    get method added for submitOffer collection to get data from database

--- index.js
+++ index.js
@@ -88,6 +88,17 @@ async function run() {
 
 
 
+app.get('/submitOffer', async (req, res) => {
+  const result = await submitOfferCollection.find().toArray();
+  res.send(result);
+})
+
+
+
+
+
+
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
